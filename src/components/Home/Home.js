c6487912e677bc34a72cf8718a8f603e15d473ee
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-11-27 23:21:05 +0600

    trying to fix bug

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { Container,Row,Col, Collapse } from 'react-bootstrap';
-import AllPizza from '../../pizza-data';
+import React,{ useEffect } from 'react';
+import { Container,Row,Col} from 'react-bootstrap';
 import Pizza from '../Pizza/Pizza';
+// import { getAllPizzas } from "";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from '../../redux/actions/pizzaAction';
 const Home = () => {
+    const dispatch = useDispatch();
+  const pizzaState = useSelector((state) => state.getAllPizzaReducer);
+  const { loading, pizzas, error } = pizzaState;
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, [dispatch]);
     return (
         <>
             <Container>
-                <Row>
-                    {
-                        AllPizza.map((pizza)=>(
-                            <Col md={4}>
-                                <Pizza pizza={pizza} />
-                            </Col>
-                        ))
-                    }
-                </Row>
-            </Container>
+        {loading ? (
+          <h1>Loading ...</h1>
+        ) : error ? (
+          <h1>Error while fetching pizzas</h1>
+        ) : (
+          <Row>
+            {pizzas.map((pizza) => (
+              <Col md={4}>
+                <Pizza pizza={pizza} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
         </>
     );
 };
